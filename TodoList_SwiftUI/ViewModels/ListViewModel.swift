c0c112181e1a29eeade44027b5c26504c9840aac
Hanbit Kim <gksqlc6901@naver.com>
2024-07-23 17:05:17 +0900

    저장기능 - UserDefault

--- TodoList_SwiftUI/ViewModels/ListViewModel.swift
+++ TodoList_SwiftUI/ViewModels/ListViewModel.swift
@@ -20,19 +20,38 @@ import SwiftUI
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        //saveItems 함수를 각 함수에 즉, 추가, 수정, 업데이트할 때마다 넣어주는 것을 비효율적이기 때문에 변화에 따라서 함수가 실행되도록
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "first item", isCompleted: false),
-            ItemModel(title: "second item", isCompleted: true),
-            ItemModel(title: "third item", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+        //임시 데이터 불러오기
+//        let newItems = [
+//            ItemModel(title: "first item", isCompleted: false),
+//            ItemModel(title: "second item", isCompleted: true),
+//            ItemModel(title: "third item", isCompleted: false)
+//        ]
+//        items.append(contentsOf: newItems)
+        
+        //UserDefaults에서 데이터 불러오기
+//        let data = UserDefaults.standard.data(forKey: itemsKey)
+        
+        //UserDefault가 비어있을 경우를 대비해서 guard let으로 선언
+        guard 
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -58,6 +77,12 @@ class ListViewModel: ObservableObject {
         //위에 내용과 동일하지만 더 간결
         if let index = items.firstIndex(where: { $0.id == item.id}) {
             items[index] = item.updateCompletion() 
+        }
+    }
+    
+    func saveItems() { //UserDefault를 사용해서 plist에 저장
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
